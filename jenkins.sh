0eaf9ac4e485c96e2c4739a53e4b245f33877e14
GautamKumar <kodukulagautamkumar@gmail.com>
2025-08-19 04:43:32 +0530

    jenkins

---
+++ jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+growpart /dev/nvme0n1 4
+
+lvextend -L +10G /dev/RootVG/rootVol
+lvextend -L +10G /dev/mapper/RootVG-varVol
+lvextend -1 +100%FREE /dev/mapper/RootVG-varTmpVol
+
+xfs_growfs /
+xfs_growfs /var/tmp
+xfs_growfs /var
+
+curl -o /etc/yum.repos.d/jenkins.repo \https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install fontconfig java-21-openjdk  -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl start jenkins
+systemctl enable jenkins
